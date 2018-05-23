09a93b72189714a7aaf22494ffb42caf19f6c3d7
Mircea Dinoiu <mircea.dinoiu@gmail.com>
2018-05-23 16:22:12 -0700

    Git improvements

--- git.sh
+++ git.sh
@@ -24,8 +24,18 @@ git-pushf() {
   git push -f -u origin $(git branch | sed -n '/\* /s///p')
 }
 
+git-deletelocal() {
+  git fetch --prune
+  git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' |  xargs git branch -d -f
+}
+
+git-notpushed() {
+  git branch -vv | cut -c 3- | awk '$3 !~/\[/ { print $1 }'
+}
+
 # Aliases
 alias git-amend='git commit -a --amend --no-edit'
 alias git-afp='git-amend && git-pushf'
 alias git-pullr='git pull origin master --rebase'
-alias git-delete-merged='git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d'
+alias git-pushall='git push --all'
+alias git-deletemerged='git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d'
