b5caac9ef4a58c239cab1b6968a3113bfecbeb75
Mircea Dinoiu <mircea@farmersbusinessnetwork.com>
2024-01-13 18:31:13 -0800

    MySalesLiteDashboard

--- profile.sh
+++ profile.sh
@@ -97,17 +97,5 @@ sizegz() {
 alias httpserver='http-server -S -p 3000 public'
 alias addDockSep='defaults write com.apple.dock persistent-apps -array-add '"'"'{ "tile-type" = "spacer-tile"; }'"'"'; killall Dock'
 
-# Shell completion
-if type brew &>/dev/null; then
-  HOMEBREW_PREFIX="$(brew --prefix)"
-  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
-    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
-  else
-    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
-      [[ -r "$COMPLETION" ]] && source "$COMPLETION"
-    done
-  fi
-fi
-
 export NODE_OPTIONS=--max_old_space_size=8192
 export PATH="$HOME/.yarn/bin:$PATH"
